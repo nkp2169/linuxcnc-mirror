25767fde6b619783699cf81383435e15d8d729f9
Robert W. Ellenberg <rwe24g@gmail.com>
2015-03-30 10:53:22 -0400

    tp: for testing, allow stepping in reverse direction
    Signed-off-by: Robert W. Ellenberg <rwe24g@gmail.com>

--- src/emc/tp/tp.c
+++ src/emc/tp/tp.c
@@ -3274,8 +3274,6 @@ int tpResume(TP_STRUCT * const tp)
         return TP_ERR_FAIL;
     }
     tp->pausing = 0;
-    //FIXME Is this call ignored with non-zero velocity? If so, it will cause a crash
-    tpSetRunDir(&emcmotDebug->coord_tp, TC_DIR_FORWARD);
     return TP_ERR_OK;
 }
 
