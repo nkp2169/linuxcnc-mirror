LinuxCNC/linuxcnc

task: Add forward command to complement reverse command
This fix also adds stronger prohibitions against switching motion when
not moving, so that we don't clobber motion by suddenly moving forward
during a reverse move.

Signed-off-by: Robert W. Ellenberg <rwe24g@gmail.com>

src/emc/motion/command.c
@@ -1042,8 +1042,17 @@ check_stuff ( "before command_handler()" );
 	    /* run motion in reverse*/
 	    /* only allowed during a pause */
 	    rtapi_print_msg(RTAPI_MSG_DBG, "REVERSE");
-        if (emcmotStatus->paused == 1) {
+        if (emcmotStatus->paused == 1 && emcmotStatus->current_vel == 0.0) {
             tpSetRunDir(&emcmotDebug->tp, TC_DIR_REVERSE);
+        }
+	    break;
+
+	case EMCMOT_FORWARD:
+	    /* run motion in reverse*/
+	    /* only allowed during a pause */
+	    rtapi_print_msg(RTAPI_MSG_DBG, "FORWARD");
+        if (emcmotStatus->paused == 1 && emcmotStatus->current_vel == 0.0) {
+            tpSetRunDir(&emcmotDebug->tp, TC_DIR_FORWARD);
         }
 	    break;
 

nkp2169/linuxcnc-mirror

src/emc/motion/command.c
@@ -1174,8 +1174,17 @@ void emcmotCommandHandler(void *arg, long period)
 	    /* run motion in reverse*/
 	    /* only allowed during a pause */
 	    rtapi_print_msg(RTAPI_MSG_DBG, "REVERSE");
-        if (emcmotStatus->paused == 1) {
+        if (emcmotStatus->paused == 1 && emcmotStatus->current_vel == 0.0) {
             tpSetRunDir(&emcmotDebug->coord_tp, TC_DIR_REVERSE);
+        }
+	    break;
+
+	case EMCMOT_FORWARD:
+	    /* run motion in reverse*/
+	    /* only allowed during a pause */
+	    rtapi_print_msg(RTAPI_MSG_DBG, "FORWARD");
+        if (emcmotStatus->paused == 1 && emcmotStatus->current_vel == 0.0) {
+            tpSetRunDir(&emcmotDebug->coord_tp, TC_DIR_FORWARD);
         }
 	    break;
 